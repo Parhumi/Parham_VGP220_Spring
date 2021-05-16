863e8d47e4c235a20c6f82a4cc5a65596c170fd6
Parham Ebadi <parhame10@gmail.com>
2021-05-15 18:02:31 -0700

    - Began working on the Assignment_04

--- Assignment_04/Queue.h
+++ Assignment_04/Queue.h
@@ -7,26 +7,64 @@ template<typename T>
 class Queue 
 {
 public:
-	Queue();
-	~Queue();
+	Queue(int capacity) : mCapacity(capacity)
+	{
+		mData = new T[capacity];
+	}
+	~Queue()
+	{
+		delete[] mData;
+		mData = nullptr;
+	}
 	//Inserts the data in the queue.
-	void Enqueue(T data);
+	void Enqueue(T data)
+	{
+		if (mCurrentSize != mCapacity)
+		{
+			mData[rear] = data;
+
+			rear++;
+			rear %= mCapacity;
+			mCurrentSize++;
+		}
+	}
 	
 	//Removes an element from the queue and returns this element in the output parameter
 	//Also it returns true if the object was removed and false if the Queue is empty
-	bool Daqueue(T& output);
+	bool Daqueue(T& output)
+	{
+		if (mCurrentSize > 0)
+		{
+			output = mData[front];
+			front++;
+			front %= mCapacity;
+			mCurrentSize--;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 	
 	//Returns true if queue is empty, false otherwise
-	bool isEmpty();
+	bool isEmpty()
+	{
+		return mCurrentSize == 0;
+	}
 
 	//Display all the elements in the queue:
 	//Example, lets suppose you inserted 1, 2, 3, 4, 5
 	//Display should print: "front->1, 2, 3, 4, 5<-rear"
-	void DisplayQueue();
+	void DisplayQueue()
+	{
+
+	}
+
 private:
-	int front;
-	int rear;
-	int mCurrentSize;
-	int mCapacity;
-	T* mData;
+	int front = 0;
+	int rear = 0;
+	int mCurrentSize = 0;
+	int mCapacity = 10;
+	T* mData = nullptr;
 };
